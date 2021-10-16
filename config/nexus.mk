ba1c65260fc33205cdc4e2caf4eabdaa939d9ee7
Vinay Verma <vvinay@codeaurora.org>
2021-10-16 05:27:57 +0200

    config: Disable Async MTE on System Server
    Async mode MTE is enabled by default on System Server process.
    Add property arm64.memtag.process.system_server=off
    This property is read during system server forking process and
    accordingly Heap Tagging level is set.
    
    Change-Id: I5ff384601f15ae5ef3dfcb6c02832843c6f2942c

--- config/nexus.mk
+++ config/nexus.mk
@@ -1,3 +1,7 @@
+# Disable async MTE on system_server
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    arm64.memtag.process.system_server=off
+
 # Disable RescueParty due to high risk of data loss
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.disable_rescue=true
