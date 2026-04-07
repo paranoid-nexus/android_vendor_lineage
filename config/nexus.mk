cd6f8a5a89a2cc47de4ac413d655d2b1507f13ff
CuriousNom <aryanbiswas7585@gmail.com>
2026-04-07 08:43:56 +0530

    overlay: Add pinner service configuration
    * Current head @a2a7755 from AOSPAFrameworkOverlay
    * Also set correct pinner service for SystemUI
    
    Change-Id: Iaa94b917ccfd79e03e3f02c709897b497dd0ef2c

--- config/nexus.mk
+++ config/nexus.mk
@@ -24,6 +24,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0.vendor
 
+# Overlay
+PRODUCT_PACKAGES += \
+    FrameworkOverlayNexus
+
 # Protobuf - Workaround for prebuilt Qualcomm HAL
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
