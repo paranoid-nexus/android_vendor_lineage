3ac1fd3316fb312faccbfd1b0180529113155cb9
Vishalcj17 <vishalcj@aospa.co>
2026-04-21 15:41:36 +0530

    config: Build QTI Framework detection packages
    * Also build VNDFWK system and vendor variants.
    
    Change-Id: I7d415d2eb38419cce3e3d81318133d662c1fe763

--- config/nexus.mk
+++ config/nexus.mk
@@ -8,6 +8,17 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0.vendor
 
+# QTI VNDK Framework Detect
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect_system \
+    libqti_vndfwk_detect_vendor \
+    libvndfwk_detect_jni.qti_system \
+    libvndfwk_detect_jni.qti_vendor \
+    libvndfwk_detect_jni.qti.vendor \
+    libqti_vndfwk_detect.vendor
+
 # Qualcomm Common
 $(call inherit-product, device/qcom/common/common.mk)
 
