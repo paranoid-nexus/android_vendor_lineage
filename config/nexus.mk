c73062477900beb4c0d19ea4b63b67763e92306c
Vishalcj17 <vishalcj@aospa.co>
2026-04-21 15:38:55 +0530

    config: Build HIDL base and manager
    Change-Id: I4775c26858f29f9f7f7ef5a618b36ef67048f5ea

--- config/nexus.mk
+++ config/nexus.mk
@@ -1,6 +1,13 @@
 # Enable aosp packages allowlist
 PRODUCT_PACKAGES += initial-package-stopped-states-aosp.xml
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0.vendor
+
 # Qualcomm Common
 $(call inherit-product, device/qcom/common/common.mk)
 
